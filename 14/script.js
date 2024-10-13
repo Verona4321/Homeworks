92a25d65fefaee292420cae6b21865111b302079
Verona4321 <sverona432@yandex.ru>
2024-10-14 02:29:31 +0400

    last task

--- 14/script.js
+++ 14/script.js
@@ -22,3 +22,16 @@ console.log(filteredPositive);
 
 const filteredNegative = grades.filter(el=>el < 60);
 console.log(filteredNegative); 
+
+
+
+
+const secondArr = grades.map(function (item) { //Берём каждый элемент массива
+  if (item >= 80) {return 'A';}
+  if (item >= 60 & item < 80) {return 'B';}
+  if (item >= 40 & item < 60) {return 'C';}
+  if (item >= 20 & item < 40) {return 'D';}
+  if (item < 20) {return 'E';}
+}); 
+
+console.log(secondArr); 
